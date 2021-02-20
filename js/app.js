e8702f32b0417ae091f2ac00bc1a8b8f0547f6d6
NamanGoyal07 <naman18@iitg.ac.in>
2021-02-20 13:59:55 +0530

    Setup is ok

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = 'Iv1.0be42ec2662d1704';
+const client_secret = 'c8db79e5dacfebc049d2bde63720b7d77bfb3a11';
+
+const fetchUsers = () => {
+    
+}
+
+searchButton.addEventListener('click', () =>{
+    console.log("Hello!");
+})
+
+
