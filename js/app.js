9fe7f8a43e911784f1e21a437ff4b86d3cb4b7d4
NamanGoyal07 <naman18@iitg.ac.in>
2021-02-21 02:33:33 +0530

    bug in name display

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const inputValue = document.querySelector("#search");
 const searchButton = document.querySelector(".searchButton");
-const nameContainer = document.querySelector(".main__profile-name");
-const unContainer = document.querySelector(".main__profile-username");
+let nameContainer = document.querySelector(".main__profile-name");
+let unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
 
@@ -23,7 +23,7 @@ const showData = () => {
     fetchUsers(inputValue.value).then((res) => {
         console.log(res);
 
-        
+        nameContainer.innerHTML = `Name: <span class="main__profile-value> ${res.data.name}</span>`;
 
     });
 };
