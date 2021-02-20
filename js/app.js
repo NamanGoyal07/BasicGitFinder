22704879ed7a511cf282c122839a31c99628afd0
NamanGoyal07 <naman18@iitg.ac.in>
2021-02-20 14:11:44 +0530

    Fetch data from github

--- js/app.js
+++ js/app.js
@@ -8,8 +8,15 @@ const urlContainer = document.querySelector(".main__profile-url");
 const client_id = 'Iv1.0be42ec2662d1704';
 const client_secret = 'c8db79e5dacfebc049d2bde63720b7d77bfb3a11';
 
-const fetchUsers = () => {
-    
+// Function to call API from Github
+const fetchUsers = async(user) => {
+    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+
+    const data = await api_call.json();
+    return {
+        data: data 
+        // Or we can do just "data" if the value and name have same variable name
+    }
 }
 
 searchButton.addEventListener('click', () =>{
